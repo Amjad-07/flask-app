0861beb163c5e43f8d7492820c0aea0366deee73
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2025-06-12 10:17:54 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 apt update
 apt install -y docker.io
 gcloud auth configure-docker
-docker pull us-central1-docker.pkg.dev/[PROJECT_ID]/my-repo/flask-app:latest
-docker run -d -p 80:8080 us-central1-docker.pkg.dev/[PROJECT_ID]/flask-app:latest
+docker pull us-central1-docker.pkg.dev/genuine-flight-462611-t5/my-repo/flask-app:latest
+docker run -d -p 80:8080 us-central1-docker.pkg.dev/genuine-flight-462611-t5/flask-app:latest
