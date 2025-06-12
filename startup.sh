24f5d34e896a2a5bc9aa54c99baa922fc5096075
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2025-06-12 09:57:06 +0530

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update
+apt install -y docker.io
+gcloud auth configure-docker
+docker pull us-central1-docker.pkg.dev/[PROJECT_ID]/my-repo/flask-app:latest
+docker run -d -p 80:8080 us-central1-docker.pkg.dev/[PROJECT_ID]/flask-app:latest
